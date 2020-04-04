a85d8f455c13f6111fba9e248ca5aef0698f22d7
yujunliu95 <yujunliu95@gmail.com>
2020-04-04 18:01:26 +0800

    4/4

--- 1.1_helloworld/HelloWorld.java
+++ 1.1_helloworld/HelloWorld.java
@@ -1,5 +1,12 @@
+	// 单行注释
+	// class-类
 public class HelloWorld {
+	// 万年不变，代表main
+		//代表程序起点
 	public static void main(String[] args) {
-		System.out.println("Hello,World!");
+		//相当于python的print()
+		System.out.println("Hello,World!!!");
 	}
 }
+
+关键字
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # RE0_JAVA
-### 此仓库将记录本菜鸡从0开始学习JAVA的所有过程
+#### 此仓库将记录本菜鸡从0开始学习JAVA的所有过程
+* 1.1 Helloword
+    > 关键字: public, static void, class
