aaa04fe2279b4a0ce5ebde60e808c16b332669f3
yujunliu95 <yujunliu95@gmail.com>
2020-04-04 22:08:58 +0800

    4/4

--- 1.0-2.0/Demo02DataType.java
+++ 1.0-2.0/Demo02DataType.java
@@ -1,5 +1,5 @@
 public class Demo01DataType {
-    public static void  main(Stringp[] args) {
+    public static void  main(String[] args) {
 
         // right(long) -----> left(int) will return error
         // int num = 100L;
@@ -7,6 +7,9 @@ public class Demo01DataType {
         int num = (int) 100L;
         System.out.println(num);
 
+
+        int num2 = (int) 60000000000L;
+        System.out.println(num2); //1705032704
 
 
     }
