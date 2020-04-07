a79495d5e92bb52b584faa72fc75d9bf3353631d
yujunliu95 <yujunliu95@gmail.com>
2020-04-07 21:50:49 +0800

    4/7

---
+++ 1.0-2.0/Demo09Logic.java
@@ -0,0 +1,17 @@
+public class Demo09Logic {
+    public static void main(Stringp[] args) {
+        System.out.println(true && false); //false
+
+        int a = 10;
+        System.out.println(3 > 4 && ++a < 100); // false
+        System.out.println(a); //10
+
+
+
+
+
+
+
+
+    }
+}
