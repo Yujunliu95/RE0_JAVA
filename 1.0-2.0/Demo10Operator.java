9c18e899c23672d22941c2d1da67be9b95dd3948
yujunliu95 <yujunliu95@gmail.com>
2020-04-12 23:40:24 +0800

    4/12更新

---
+++ 1.0-2.0/Demo10Operator.java
@@ -0,0 +1,11 @@
+public class Demo10Operator {
+    public static void main(Stringp[] args) {
+        int a = 10;
+        int b = 20;
+
+        int max =  a > b ? a : b                       // 最大值的变量
+        // 判断a > b 是否成立，如果成立a的值赋值给max，不成立b的值赋值给max
+        System.out.println("最大值:  "  +  max);       // 20
+        int result = 3 > 4 ?
+    }
+}
